7e14da99d3746da4e87390fcb42ac0c28c208e15
Vadim <romazanovvadim2003@gmail.com>
2024-12-11 12:37:33 +0300

    fix: merge

--- webgl-earth-test/src/stores/satellites/satellite-store.ts
+++ webgl-earth-test/src/stores/satellites/satellite-store.ts
@@ -2,22 +2,10 @@ import { makeAutoObservable, runInAction } from "mobx";
 import { SatelliteService } from "../../api";
 import { ISatellite } from "../../types";
 
-export class SatelliteStore {
+class SatelliteStore {
     satellites: ISatellite[] = [];
-    selectedSatellites: ISatellite[] = [];
-    isLoading: boolean = false;
 
     constructor() {
         makeAutoObservable(this);
-    }
-
-    async fetchSatellites() {
-        this.isLoading = true;
-        const data = await SatelliteService.fetchSatellites(); 
-        
-        runInAction(() => {
-            this.satellites = data;
-            this.isLoading = false;
-        });
     }
 }
